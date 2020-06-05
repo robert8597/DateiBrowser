f8dc65bbe59a52ba98ca701d8051f8629e405cf2
robert8597 <66479327+robert8597@users.noreply.github.com>
2020-06-05 18:37:25 +0200

    Add files via upload
    Attribute anzeigen

--- DateiBrowserBeta.sh
+++ DateiBrowserBeta.sh
@@ -1,34 +1,49 @@
 #!/bin/bash
 #dialog --title "text" --fselect /path/to/dir height width
 
-FILE=$(dialog --title "Datei auswählen" --stdout --title "Kopieren/Löschen/Öffnen" --fselect /home/r/Schreibtisch 14 48)
+FILE=$(dialog --title "Datei auswählen" --stdout --title "Kopieren/Löschen/Öffnen" --fselect /home/$USER/Schreibtisch 14 48)
+oeDatum=$(date -d "@$( stat -c '%X' $FILE )" +'%F %T ') #Letztes Öffnen von Datei DATUM
+LDatum=$(date -d "@$( stat -c '%Y' $FILE )" +'%F %T %z') #letzte Änderung DATUM
+Rechte=$(ls -l $FILE)
+Dateityp=$(file $FILE)
 
-
-AUSWAHL=$(dialog --menu "AUSWAHL" --stdout 14 48 10 "1" "Datei öffnen" "2" "Datei löschen" "3" "Datei kopieren" "4" "Datei verschieben" "5" "Datei größe anzeigen" "6" "Ordner Inhalt anzeigen")
+AUSWAHL=$(dialog --menu "AUSWAHL" --stdout 14 48 10 "1" "Datei öffnen" "2" "Datei löschen" "3" "Datei kopieren" "4" "Datei verschieben" "5" "Datei Attribute anzeigen" "6" "Ordner Inhalt anzeigen")
 clear
 echo "$AUSWAHL"
 
+
+
 case "$AUSWAHL" in
-1) echo "Ausgewählte Datei wird geöffnet" 
+1) 
+echo "Ausgewählte Datei wird geöffnet" 
 xdg-open $FILE 
 ;;
+
 2) rm $FILE
 echo "Ausgewählte Datei wurde gelöscht";;
-3) Ordner=$(dialog --title " auswählen" --stdout --title "Zielordner auswählen" --dselect /home/r/Schreibtisch 14 48 )
+3) Ordner=$(dialog --title " auswählen" --stdout --title "Zielordner auswählen" --dselect /home/$USER/Schreibtisch 14 48 )
 cp $FILE $Ordner
 clear
 echo "Datei wurde kopiert";;
-4) Ordner=$(dialog --title " auswählen" --stdout --title "Zielordner auswählen" --dselect /home/r/Schreibtisch 14 48 )
+4) Ordner=$(dialog --title " auswählen" --stdout --title "Zielordner auswählen" --dselect /home/$USER/Schreibtisch 14 48 )
 mv $FILE $Ordner
 clear
 echo "Datei wurde verschoben";;
-5) TEST=$(stat -c %s $FILE)
-dialog --msgbox "Die Datei ist $TEST Bytes groß" 14 48
-echo "Die Datei ist" $TEST "Bytes groß";;
-6)Ordner=$(dialog --title " auswählen" --stdout --title "Zielordner auswählen" --dselect /home/r/Schreibtisch 14 48 )
+5) bytegroesse=$(stat -c %s $FILE)
+#MB=$(($bytegroesse/100))
+dialog --msgbox "Die Datei ist $bytegroesse bytes groß   
+Pfad = $FILE
+Letzter Zugriff =  $oeDatum
+Letzte Änderung  = $LDatum
+Rechte = $Rechte
+Dateityp = $Dateityp" 14 48
+
+
+;;
+6)Ordner=$(dialog --title " auswählen" --stdout --title "Zielordner auswählen" --dselect /home/$USER/Schreibtisch 14 48 )
 Liste=$(ls -ld $Ordner/*)
 echo $Liste
-#dialog --title "Ordner" --msgbox "TEST"
+
 ;;
 esac
  
